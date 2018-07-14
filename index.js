1f6ebc9ae977ac6815d882b4065d12f154061217
Suley Mahmoud <mahmoudsuley@hotmail.com>
2018-07-15 00:20:59 +0100

    primative types

---
+++ index.js
@@ -0,0 +1,14 @@
+// Comment on the code about first javascript code!
+console.log('hi again world');
+let name = 'Suley'; //string literal
+let surname = 'Mahmoud';
+let age = 30; // number literal
+let isApproved = true; //boolean literal
+let firstName = null; // null
+console.log(name);
+console.log(surname);
+const interestRate = 0.3;
+console.log(age);
+console.log(isApproved);
+console.log(firstName);
+console.log(interestRate);
